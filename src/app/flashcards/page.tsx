0ab1fe0fa28834468570a306b2ffd96b2d7a5e91
znrfrnz <franzbaes69@gmail.com>
2026-04-18 18:13:08 +0800

    feat(02-03): convert /flashcards to client component reading from Zustand store

--- src/app/flashcards/page.tsx
+++ src/app/flashcards/page.tsx
@@ -1,6 +1,10 @@
+﻿'use client'
 import { StudySession } from '@/components/StudySession'
 import { FLASHCARDS } from '@/data/flashcards'
+import { useFlashcardStore } from '@/stores/flashcardStore'
 
 export default function FlashcardsPage() {
-  return <StudySession cards={FLASHCARDS} />
+  const generatedCards = useFlashcardStore((s) => s.generatedCards)
+  const cards = generatedCards.length > 0 ? generatedCards : FLASHCARDS
+  return <StudySession cards={cards} />
 }
