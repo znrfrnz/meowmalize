8bd6d8ded3c9288c28fed1b913eb00a27d830ae9
znrfrnz <franzbaes69@gmail.com>
2026-04-18 19:07:18 +0800

    fix(05): add useDeckStore rehydration to StorageHydration

--- src/components/StorageHydration.tsx
+++ src/components/StorageHydration.tsx
@@ -1,10 +1,12 @@
 'use client'
 import { useEffect } from 'react'
 import { useFlashcardStore } from '@/stores/flashcardStore'
+import { useDeckStore } from '@/stores/deckStore'
 
 export function StorageHydration() {
   useEffect(() => {
     useFlashcardStore.persist.rehydrate()
+    useDeckStore.persist.rehydrate()
   }, [])
   return null
 }
